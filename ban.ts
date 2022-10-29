d5a785611b85660ec00772dd1c46e61cf378ba27
runekaito <115391871+runekaito@users.noreply.github.com>
2022-10-29 11:18:43 +0900

    Update ban.ts

--- ban.ts
+++ ban.ts
@@ -1,47 +1,78 @@
+import {
+  CommandPermissionLevel,
+  PlayerCommandSelector,
+} from "bdsx/bds/command";
 import { command } from "bdsx/command";
-import { ipfilter } from "bdsx/core";
-import { CxxString, int32_t } from "bdsx/nativetype";
-import { connectionList } from "./example_and_test/net-login";
+import { CommandResultType } from "bdsx/commandresult";
+import { bedrockServer } from "bdsx/launcher";
 import * as fs from "fs";
+import { connectionList } from "./net-login";
 import { events } from "bdsx/event";
-import { getNameOfDeclaration } from "typescript";
-import { CommandPermissionLevel } from "bdsx/bds/command";
+import { ipfilter } from "bdsx/core";
+let path = "./ban.json";
+if (!fs.existsSync(path))
+  fs.writeFileSync(path, JSON.stringify({ masterData: [] }), "utf8");
 
-let jsonObject: any;
-var masterData: {
-  name: string;
-  ip: any;
-}[] = [];
-let jsi: object;
-command.register("ban", "ip ban").overload(
-  (p, o, op) => {
-    for (const ni of connectionList.keys()) {
-      const actor = ni.getActor();
-      if (actor?.isPlayer() && actor?.getName() == p.name) {
-        var ip1 = ni.getAddress();
-        var data = {
-          name: actor.getName(),
-          ip: ip1,
-        };
-        masterData.push(data);
-        let masterData2: string = JSON.stringify({ masterData }, null, " ");
-        fs.writeFileSync("./ban-ip.json", masterData2);
-        ipfilter.add(ip1);
+command
+  .register(
+    "ban",
+    "ipBan" /* Command permision */,
+    CommandPermissionLevel.Operator
+  )
+  .overload(
+    (p, o, op) => {
+      const result = bedrockServer.executeCommand(
+        "kick " + p.target.getName(),
+        CommandResultType.Data
+      );
+      let data: any;
+      if (result.result == 1) {
+        for (const ni of connectionList.keys()) {
+          const actor = ni.getActor();
+          if (actor?.isPlayer() && actor?.getName() == p.target.getName()) {
+            data = {
+              ip: ni.getAddress().split("|")[0],
+              name: actor.getName(),
+            };
+            bedrockServer.executeCommand(
+              `tellraw @a {"rawtext":[{"text":"§l§f${o
+                .getEntity()
+                ?.getName()} §ckicked §f${actor.getName()}"}]}`
+            );
+            ipfilter.add(ni.getAddress().split("|")[0]);
+          }
+        }
+        let data_arr: any[] = JSON.parse(
+          fs.readFileSync(path, "utf8")
+        ).masterData;
+        data_arr.push(data);
+        let masterData: string = JSON.stringify(
+          { masterData: data_arr },
+          null,
+          " "
+        );
+        fs.writeFileSync(path, masterData);
       }
+    },
+    {
+      target: PlayerCommandSelector,
     }
-  },
-  {
-    name: CxxString,
-  }
-);
-command.find("ban").signature.permissionLevel = CommandPermissionLevel.Operator;
-events.serverOpen.on(() => {
-  jsonObject = JSON.parse(fs.readFileSync("./ban-ip.json", "utf8"));
-  jsi =
-    jsonObject.masterData.filter(() => {
-      true;
-    }) || {};
-  for (const i in jsi) {
-    ipfilter.add(jsi[i].ip);
+  );
+
+events.playerJoin.on((ev) => {
+  let jsonObject = JSON.parse(fs.readFileSync(path, "utf8")).masterData;
+  for (const ni of connectionList.keys()) {
+    const actor = ni.getActor();
+    if (actor?.getName() == ev.player.getName()) {
+      jsonObject.filter((item: any) => {
+        if (ni.getAddress().split("|")[0] == item.ip) {
+          bedrockServer.executeCommand(
+            `kick "${ev.player.getName()}"`,
+            CommandResultType.Data
+          );
+          ipfilter.add(item.ip);
+        }
+      });
+    }
   }
 });
